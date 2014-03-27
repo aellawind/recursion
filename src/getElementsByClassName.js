a8117d6476fc6b29e77bb1ddef7bd8b071b8e0ff
Amira Anuar <amiraanuar@gmail.com>
2014-03-26 17:53:23 -0700

    Gets all elements by class recrusively, completed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,7 +3,22 @@
 //   return document.getElementsByClassName(className);
 // };
 
-// But in stead we're going to implement it from scratch:
+// But instead we're going to implement it from scratch:
+// Directions say to used document.body, element.childNodes, and element.classList
 var getElementsByClassName = function (className) {
-  // your code here
+
+	var allElements = []; //results array
+	var getElements = function(node) {
+		var childNodes = node.childNodes; //getting all the child nodes to iterate over them and check
+		for (var i=0; i<childNodes.length;i++) {
+			console.log(childNodes[i]);
+			//have to remember to check if the node even has any classes in its classList
+			if(childNodes[i].classList && childNodes[i].classList.contains(className)) {
+				allElements.push(childNodes[i]); //if the element has the class, add it to results list
+			}
+			getElements(childNodes[i], className); //recursively check the child node too
+		}
+		return allElements;	
+	}
+	return getElements(document.body);
 };
